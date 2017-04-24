c3aeae0c45a37f192c34e9527bc37b2b3862487b
Barlo <dkrzus92@gmail.com>
2017-04-24 21:44:43 +0200

    Create mongo db context

---
+++ DrinkUp.WebApi/DrinkUp.WebApi/Context/MongoContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DrinkUp.WebApi.Model;
+using MongoDB.Bson;
+using MongoDB.Shared;
+using MongoDB.Driver;
+
+namespace DrinkUp.WebApi.Context {
+    public interface IMongoContext {
+        IQueryable<Drink> GetAll();
+        void Insert(Drink drink);
+    }
+
+    public class MongoContext : IMongoContext
+    {
+        private readonly MongoDatabaseBase db;
+        private readonly MongoClient client;
+
+        public MongoContext() {
+            client = new MongoClient("mongodb://localhost:27017");
+            db = client.GetDatabase("DrinkUpDb") as MongoDatabaseBase;
+        }
+
+        public IQueryable<Drink> GetAll() {
+            return GetDrinkCollection()
+                .AsQueryable();
+        }
+
+        public void Insert(Drink drink) {
+            GetDrinkCollection()
+                .InsertOne(drink);
+        }
+
+        private IMongoCollection<Drink> GetDrinkCollection() {
+            return db.GetCollection<Drink>("Drinks");
+        }
+    }
+}
---
+++ DrinkUp.WebApi/DrinkUp.WebApi/Model/Drink.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DrinkUp.WebApi.Model {
+    public class Drink {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<string> Ingredients { get; set; }
+        public string Glass { get; set; }
+        public int IsGood { get; set; }
+        public int IsBad { get; set; }
+        public int TopCount{ get; set; }
+        public string Description { get; set; }
+        public IList<string> Comments { get; set; }
+        public IList<string> Images { get; set; }
+    }
+}
