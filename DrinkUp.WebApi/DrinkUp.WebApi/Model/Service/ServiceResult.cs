30ff418ec9828d23e632c8e6d0204b6b257251c7
Barlo <dkrzus92@gmail.com>
2017-04-29 12:40:35 +0200

    Add repsonse resolver and service result

---
+++ DrinkUp.WebApi/DrinkUp.WebApi/Model/Service/ServiceResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkUp.WebApi.Model.Service
+{
+    public class ServiceResult
+    {
+        public bool IsValid { get; private set; }
+
+    }
+
+    public class ServiceResult<T> {
+        public T Data { get; set; }
+    }
+}
