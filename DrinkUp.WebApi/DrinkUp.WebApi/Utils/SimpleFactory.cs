14c60ad9aec086b5912efa43c9f82c42a0efb82d
Barlo <dkrzus92@gmail.com>
2017-05-16 22:44:09 +0200

    Add method for create service result to simple factory

--- DrinkUp.WebApi/DrinkUp.WebApi/Utils/SimpleFactory.cs
+++ DrinkUp.WebApi/DrinkUp.WebApi/Utils/SimpleFactory.cs
@@ -1,7 +1,11 @@
-﻿namespace DrinkUp.WebApi.Utils {
+﻿using DrinkUp.WebApi.Model.Service;
+
+namespace DrinkUp.WebApi.Utils {
     public static class SimpleFactory<T> where T : new() {
-        public static T Create() {
-            return new T();
-        }
+        public static T Create() => new T();
+
+        public static ServiceResult CreateResult() => new ServiceResult();
+
+        public static ServiceResult<T> CreateResultWithData => new ServiceResult<T>();
     }
 }
