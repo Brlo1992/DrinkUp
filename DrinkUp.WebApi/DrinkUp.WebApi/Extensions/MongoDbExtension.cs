6d60d8d0716e658acc20e9bc3bea50ca01d2280c
Barlo <dkrzus92@gmail.com>
2017-05-18 00:33:55 +0200

    Some changes

--- DrinkUp.WebApi/DrinkUp.WebApi/Extensions/MongoDbExtension.cs
+++ DrinkUp.WebApi/DrinkUp.WebApi/Extensions/MongoDbExtension.cs
@@ -10,7 +10,7 @@ using static DrinkUp.WebApi.Extensions.MongoBuildersExtension;
 namespace DrinkUp.WebApi.Extensions {
     public static class MongoDbExtension {
         public static ServiceResult<IQueryable<T>> GetMany<T>(this IMongoCollection<T> db) {
-            var result = SimpleFactory<ServiceResult<IQueryable<T>>>.Create();
+            var result = ResultFactory.CreateWithData<IQueryable<T>>();
             try {
                 result.Data = db.AsQueryable();
                 result.Status = nameof(Status.ManySelected);
@@ -23,7 +23,7 @@ namespace DrinkUp.WebApi.Extensions {
         }
 
         public static async Task<ServiceResult<T>> GetSingle<T>(this IMongoCollection<T> db, string id) where T : new() {
-            var result = SimpleFactory<ServiceResult<T>>.Create();
+            var result = ResultFactory.CreateWithData<T>();
             try {
                 var queryResult = await db.Find(GetById<T>(id)).ToListAsync();
                 if (queryResult.IsOneSelected()) {
@@ -42,7 +42,7 @@ namespace DrinkUp.WebApi.Extensions {
         }
 
         public static async Task<ServiceResult> TryInsert<T>(this IMongoCollection<T> db, T item) where T : IEntity {
-            var result = SimpleFactory<ServiceResult<T>>.Create();
+            var result = ResultFactory.Create();
             try {
                 var queryResult = await db.Find(GetByName<T>(item.Name)).ToListAsync();
                 if (queryResult.IsEmpty()) {
@@ -61,7 +61,7 @@ namespace DrinkUp.WebApi.Extensions {
         }
 
         public static async Task<ServiceResult> TryDelete<T>(this IMongoCollection<T> db, string id) {
-            var result = SimpleFactory<ServiceResult<T>>.Create();
+            var result = ResultFactory.Create();
             try {
                 await db.FindOneAndDeleteAsync(GetById<T>(id));
                 result.Status = nameof(Status.Removed);
@@ -74,7 +74,7 @@ namespace DrinkUp.WebApi.Extensions {
         }
 
         public static async Task<ServiceResult> TryUpdate<T>(this IMongoCollection<T> db,string id, UpdateDefinition<T> updateDefinition) where T : IEntity {
-            var result = SimpleFactory<ServiceResult<T>>.Create();
+            var result = ResultFactory.Create();
             try {
                 var queryResult = await db.Find(GetByName<T>(id)).ToListAsync();
                 if (queryResult.IsOneSelected()) {
--- DrinkUp.WebApi/DrinkUp.WebApi/Utils/SimpleFactory.cs
+++ DrinkUp.WebApi/DrinkUp.WebApi/Utils/SimpleFactory.cs
@@ -3,9 +3,5 @@
 namespace DrinkUp.WebApi.Utils {
     public static class SimpleFactory<T> where T : new() {
         public static T Create() => new T();
-
-        public static ServiceResult CreateResult() => new ServiceResult();
-
-        public static ServiceResult<T> CreateResultWithData => new ServiceResult<T>();
     }
 }
